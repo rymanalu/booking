baf3e3bc9bb8bb96c2f80a36e64386d352e07a3d
Roni Yusuf Manalu <rymanalu@gmail.com>
2018-06-02 15:48:20 +0700

    Remove socmed auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,16 +48,16 @@
                     </div>
                 </form>
 
-                <div class="social-auth-links text-center">
-                    <p>- OR -</p>
-                    <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i>
-                        Sign in using Facebook</a>
-                    <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-                        Google+</a>
-                </div>
+                {{--<div class="social-auth-links text-center">--}}
+                    {{--<p>- OR -</p>--}}
+                    {{--<a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i>--}}
+                        {{--Sign in using Facebook</a>--}}
+                    {{--<a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using--}}
+                        {{--Google+</a>--}}
+                {{--</div>--}}
 
-                <a href="{{ route('password.request') }}">Forgot Your Password?</a><br>
-                <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
+                {{--<a href="{{ route('password.request') }}">Forgot Your Password?</a><br>--}}
+                {{--<a href="{{ route('register') }}" class="text-center">Register a new membership</a>--}}
             </div>
         </div>
     </body>
